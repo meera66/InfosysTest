fff0c59f8794b73542d1097f26e065a872f84558
Meera <meera.dave606@gmail.com>
2019-11-18 18:43:05 -0400

    Requirement 3 : Completed unite test case.

--- ConcurrencyTest/ConcurrencyTestTests/ConcurrencyTests.swift
+++ ConcurrencyTest/ConcurrencyTestTests/ConcurrencyTests.swift
@@ -9,7 +9,24 @@ import XCTest
 
 class ConcurrencyTests: XCTestCase {
     func testloadMessage() {
-        
+        let expectation = self.expectation(description: #function)
+        var result: String?
+        let timeOutString = "Unable to load message - Time out exceeded"
+        let actualCombineString = "Hello world"
+        result = timeOutString
+        loadMessage { (message) in
+            result = message
+            expectation.fulfill()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            if expectation.expectedFulfillmentCount < 1 {
+                XCTAssertEqual(result, actualCombineString, "Successfully load message")
+            } else {
+                XCTAssertEqual(result, timeOutString, "Time out error")
+            }
+        }
+        waitForExpectations(timeout: 10)
+
     }
 
 }
